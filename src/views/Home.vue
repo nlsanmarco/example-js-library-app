788049760aeffe263f895ac5575570d9ab503ec7
Nicole Sanmarco <nicole.sanmarco@yahoo.com>
2021-10-14 19:06:50 -0400

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <div id="myID">Calendar</div>
+  </div>
+</template>
+
+<style></style>
+
+<script>
+import flatpickr from "flatpickr";
+export default {
+  data: function () {
+    return {};
+  },
+  mounted: function () {
+    let calendar = flatpickr("#myID", { inlilne: true, enableTime: true, mode: "range", minDate: "2020-01" });
+    calendar.currentMonth;
+  },
+  methods: {},
+};
+</script>
